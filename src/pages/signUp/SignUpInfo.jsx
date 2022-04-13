6121394158f417ce1f75b6ab605d9a1114f807d6
manuel-heav <manuelheav32@gmail.com>
2022-04-13 21:56:50 +0300

    signup category edited

--- src/pages/signUp/SignUpInfo.jsx
+++ src/pages/signUp/SignUpInfo.jsx
@@ -35,47 +35,47 @@ const signUpInfo = () => {
 
       <form>
         <div className='input-container'>
-          <input type='text' placeholder='Email' />
-          <input type='text' placeholder='First Name' />
-          <input type='text' placeholder='Last Name' />
+          <input type='text' placeholder='Email' required/>
+          <input type='text' placeholder='First Name' required/>
+          <input type='text' placeholder='Last Name' required/>
         </div>
 
         <div className='gender-container'>
-          <input type='radio' name='gender' id='male'/>
+          <input type='radio' name='gender' id='male' required/>
           <label for='male'>Male</label>
 
-          <input type='radio' name='gender' id='female'/>
+          <input type='radio' name='gender' id='female'required/>
           <label for='female'>Female</label>
         </div>
 
         <div className='date-container'>
-          <input type='text' placeholder='DD' />
-          <input type='text' placeholder='MM' />
-          <input className='year' type='text' placeholder='YYYY' />
+          <input type='text' placeholder='DD' required/>
+          <input type='text' placeholder='MM' required/>
+          <input className='year' type='text' placeholder='YYYY' required/>
         </div>
 
         <div className='password-container'>
-          <input type='password' placeholder='Password' />
+          <input type='password' placeholder='Password' required/>
         </div>
 
         <div className='password-container'>
-          <input type='password' placeholder='Confirm Password' />
+          <input type='password' placeholder='Confirm Password' required/>
         </div>
-      </form>
       <div className='signup_location'>
         <p>Location</p>
-        <input type='text' />
+        <input type='text' required/>
         <iframe
           src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3283.891401663795!2d38.78619897820133!3d8.997153227833236!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x209cbe597069517f!2zRWRuYSBNYWxsIC0g4Yqk4Yu14YqTIOGInuGIjQ!5e0!3m2!1sen!2suk!4v1648141410336!5m2!1sen!2suk'
           allowfullscreen=''
           loading='lazy'
         ></iframe>
       </div>
-      <div className='next_icon_container'>
+      <div type="submit" className='next_icon_container'>
         <Link to='/signup/category'>
           <NextIcon className='next_icon' />
         </Link>
       </div>
+      </form>
     </div>
   );
 };
